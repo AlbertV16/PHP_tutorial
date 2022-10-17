bc74d6f8074642fdfb5e8eea6cda4dec04e2ff50
Albert Vuong <albert.vuong16@gmail.com>
2022-10-16 23:50:47 -0600

    First php file - outputs

---
+++ 01_output.php
@@ -0,0 +1,25 @@
+<?php
+    // echo 'Hello123';
+    // Double forward slash for single line comments
+
+    /*
+        This is for multiple line comments
+    */
+
+    // echo - Output strings, numbers, html, etc
+    // echo 123, 'Hello', 10.5;
+
+    // print - Works like echo, but only single arg
+    // print 123;
+
+    // Below functions usually used for debugging
+
+    // print_r() - Print single values and arrays
+    // print_r([1,2,3]);
+
+    // var_dump() - Returns more info like data type and len
+    // var_dump(true);
+
+    // var_export() - Similar to var_dump(). Outputs string representation ofa var
+    var_export('Hello');
+?>
